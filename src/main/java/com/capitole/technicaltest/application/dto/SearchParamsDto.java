5968e0f75a71ffe497ab898cc098a62d6676daac
Camilo <camilovsqz@gmail.com>
2024-03-13 12:31:17 -0300

    chore: change SearchParamsDto

--- src/main/java/com/capitole/technicaltest/application/dto/SearchParamsDto.java
+++ src/main/java/com/capitole/technicaltest/application/dto/SearchParamsDto.java
@@ -1,5 +1,27 @@
 package com.capitole.technicaltest.application.dto;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+//TODO:DOC
+@Getter
+@Setter
+@ToString
 public class SearchParamsDto {
-	//TODO: implementar
+	@NotNull(message = "applicationDate field is required")
+	@DateTimeFormat(iso = ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
+	private Date applicationDate;
+    @Min(value = 1, message = "productIdentifier field is required")
+	private int productIdentifier;
+    @Min(value = 1, message = "brandIdentifier field is required")
+	private int brandIdentifier;
 }
+
