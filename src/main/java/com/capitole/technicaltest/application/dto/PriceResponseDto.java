6f004daf6dd89fcaed08abd3cf84a866cd921e20
Camilo <camilovsqz@gmail.com>
2024-03-13 12:34:16 -0300

    chore: change PriceResponse

--- src/main/java/com/capitole/technicaltest/application/dto/PriceResponseDto.java
+++ src/main/java/com/capitole/technicaltest/application/dto/PriceResponseDto.java
@@ -1,5 +1,19 @@
 package com.capitole.technicaltest.application.dto;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PriceResponseDto {
-	//TODO: implementar
+	private int productId;
+	private int brandId;
+	private int rateToApply;
+	private List<ApplicationDataRangeDto> applicationDataRange;
+	private double finalPrice;
 }
